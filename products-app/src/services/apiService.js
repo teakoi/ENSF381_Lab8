4538f8a52c0b5cc1950f620560aaebcbfe03c8af
Maryam <maryamsalsayed@gmail.com>
2024-03-13 21:39:29 -0600

    Step C (final)

--- products-app/src/services/apiService.js
+++ products-app/src/services/apiService.js
@@ -14,7 +14,13 @@ export const fetchProducts = async () => {
 
 // Fetches details for a single product by its ID
 export const getProductDetails = async (id) => {
-    // implement it using similar logic as fetchProducts function
+    const url = `${BASE_URL}/${id}`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch(error){
+        throw error;
+    }
 };
 
 // Deletes a product by its ID
@@ -34,5 +40,6 @@ export const addProduct = (product) => {
 
 // Edits an existing product by ID
 export const editProduct = (id, product) => {
-    // implement it using similar logic as addProduct function but use axios.put method
+    const url = `${BASE_URL}/${id}/edit`;
+    return axios.put(url, JSON.stringify(product));
 };
